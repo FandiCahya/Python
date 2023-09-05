9b77751853ff8805e3ee69190a3623b3cb1bbee8
FandiCahya <fandicahya56@gmail.com>
2023-09-05 07:38:37 +0700

    update

---
+++ Dictionary.py
@@ -0,0 +1,21 @@
+# list -> array, mengakses dengan 
+# menggunakan index
+
+data_list = ['Bambang','Marpok','Ajih']
+
+print(data_list[1])
+
+# dictionary (dict) -> associative array
+# identifier -> key
+data_dict = {
+    'key' : 'value',
+    'ba' : 'Bambang',
+    'ma' : 'Marpok',
+    'aj' : 'Ajih',
+    'no' : 100,
+    'list' : data_list
+}
+
+print(data_dict['ba'])
+print(data_dict['no'])
+print(data_dict['list'])
