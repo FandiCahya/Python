afdfcc25887b23b79952ae99d180d99bb6414889
FandiCahya <fandicahya56@gmail.com>
2023-09-05 07:18:20 +0700

    python

---
+++ Tuples_Sets.py
@@ -0,0 +1,15 @@
+# List (Menggunakan Kurung Siku)
+data_list = [1,2,3,4]
+print(f"Data list = {data_list}")
+
+# Tuples (Menggunakan kurung biasa)
+data_tuples = (5,6,7,8)
+print(f"Data Tuples = {data_tuples}")
+
+# tidak bisa dilakukan
+# data_tuples[1] = "ucup"
+# data_tuples.append(1)
+
+# Sets (Menggunakan Kurung kurawal)
+data_sets = {1,2,3,4,5,6,7,8}
+print(f"Data Sets = {data_sets}")
