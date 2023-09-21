b29e3bf1f0194c44d1ad24b883387545ef1ad519
FandiCahya <fandicahya56@gmail.com>
2023-09-21 11:44:13 +0700

    baru

--- Kuliah PBO/progs/main2.py
+++ Kuliah PBO/progs/main2.py
@@ -4,8 +4,16 @@ path.append('E:\Python\Kuliah PBO\packages')
 from extra.iota import FunI
 import extra.good.best.sigma
 from extra.good.best.tau import FunT
+import extra.good.best.sigma as sig
+import extra.good.alpha as alp
+from extra.iota import funI
+from extra.good.beta import funB
 
 print(extra.good.best.sigma.FunS())
 print(FunT())
 print(FunI())
+print(sig.funS())
+print(alp.funA())
+print(funI())
+print(funB())
 
