b22b892c8874dd1ac85d0631efac18d130b4746c
FandiCahya <fandicahya56@gmail.com>
2023-09-21 11:08:08 +0700

    up

---
+++ Kuliah PBO/progs/main2.py
@@ -0,0 +1,11 @@
+from sys import path
+path.append('E:\Python\Kuliah PBO\packages')
+
+from extra.iota import FunI
+import extra.good.best.sigma
+from extra.good.best.tau import funT
+
+print(extra.good.best.sigma.funS())
+print(funT())
+print(FunI())
+
