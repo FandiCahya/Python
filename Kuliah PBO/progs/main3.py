58ea56cff494a475ab9aff8b6bcbe97648e2a8d5
Your Name <you@example.com>
2023-09-21 11:42:03 +0700

    aku napo pe kelompok an

---
+++ Kuliah PBO/progs/main3.py
@@ -0,0 +1,11 @@
+from sys import path
+path.append('/home/alfi/Python/Kuliah PBO/packages')
+
+from extra.iota import FunI
+import extra.good.best.sigma
+from extra.good.best.tau import FunT
+
+print(extra.good.best.sigma.FunS())
+print(FunT())
+print(FunI())
+
