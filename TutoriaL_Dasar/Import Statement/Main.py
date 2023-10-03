184c46ff35aca865f3b61ad69a54d9bdf5771bbf
FandiCahya <fandicahya56@gmail.com>
2023-10-03 08:31:10 +0700

    Dasar Sudah Selesai

---
+++ TutoriaL_Dasar/Import Statement/Main.py
@@ -0,0 +1,13 @@
+# 1. untuk menyambung program dari external
+import program_print
+
+# 2. import dengan data
+import vari
+
+print(vari.data)
+
+# 3. Import Dengan Fungsi
+import fungsi
+
+hasil = fungsi.matematika(3,1)
+print(hasil)
