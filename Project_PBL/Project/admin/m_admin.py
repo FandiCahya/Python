cb9e2614f644d3285e2af1628e81c8c9149df03b
FandiCahya <fandicahya56@gmail.com>
2023-10-05 12:22:17 +0700

    baru oi

--- Project_PBL/Project/admin/m_admin.py
+++ Project_PBL/Project/admin/m_admin.py
@@ -1,36 +1,44 @@
 from sys import path
 
 path.append('E:\Python\Project_PBL\Project')
-import mahasiswa.f_mahasiswa as ms
+import mahasiswa.f_mahasiswa1 as ms
 import instansi.f_instansi as ins
 import kelompok.f_kelompok as kel
-# Fungsi utama untuk Admin
-def admin_menu():
-    while True:
-        print("===== Menu Admin =====")
-        print("1. Kelola Data Mahasiswa")
-        print("2. Kelola Data Instansi")
-        print("3. Kelola Data Kelompok")
-        print("4. Keluar")
-        pilihan = input("Pilih menu: ")
 
-        if pilihan == "1":
-            ms.menu_mahasiswa()
-        elif pilihan == "2":
-            ins.menu_instansi()
-        elif pilihan == "3":
-            kel.menu_kelompok()
-        elif pilihan == "4":
-            break
+class Admin:
+    def __init__(self):
+        pass
+
+    def admin_menu(self):
+        while True:
+            print("===== Menu Admin =====")
+            print("1. Kelola Data Mahasiswa")
+            print("2. Kelola Data Instansi")
+            print("3. Kelola Data Kelompok")
+            print("4. Keluar")
+            pilihan = input("Pilih menu: ")
+
+            if pilihan == "1":
+                ms.menu_mahasiswa()
+            elif pilihan == "2":
+                ins.menu_instansi()
+            elif pilihan == "3":
+                kel.menu_kelompok()
+            elif pilihan == "4":
+                break
+            else:
+                print("Pilihan tidak valid. Silakan coba lagi.")
+
+    def login_admin(self):
+        username = input("Masukkan username Admin: ")
+        password = input("Masukkan password Admin: ")  # Anda dapat mengimplementasikan autentikasi sesuai kebutuhan
+
+        if username == "admin" and password == "admin":  # Gantilah dengan autentikasi yang sesuai
+            self.admin_menu()
         else:
-            print("Pilihan tidak valid. Silakan coba lagi.")
+            print("Login gagal. Coba lagi.")
 
-# Fungsi login Admin
-def login_admin():
-    username = input("Masukkan username Admin: ")
-    password = input("Masukkan password Admin: ")  # Anda dapat mengimplementasikan autentikasi sesuai kebutuhan
-
-    if username == "admin" and password == "admin":  # Gantilah dengan autentikasi yang sesuai
-        admin_menu()
-    else:
-        print("Login gagal. Coba lagi.")
+# Contoh penggunaan
+if __name__ == "__main__":
+    admin = Admin()
+    admin.login_admin()
--- Project_PBL/Project/kelompok/f_kelompok.py
+++ Project_PBL/Project/kelompok/f_kelompok.py
@@ -1,70 +1,118 @@
-data_kelompok = []
-# Fungsi untuk menambah data kelompok
-def tambah_kelompok(id_kelompok, nama_instansi, lama_pkl, list_nim):
-    # Temukan instansi berdasarkan id_instansi
-    instansi = None
-    for data in data_instansi:
-        if data["nama_instansi"] == nama_instansi:
-            instansi = data
-            break
+class Instansi:
+    def __init__(self, data_instansi):
+        self.data_instansi = data_instansi
 
-    if instansi is None:
-        print("Instansi dengan nama tersebut tidak ditemukan.")
-        return
+    def cari_instansi(self, nama_instansi):
+        for data in self.data_instansi:
+            if data["nama_instansi"] == nama_instansi:
+                return data
+        return None
 
-    # Temukan mahasiswa berdasarkan NIM
-    mahasiswas_kelompok = []
-    for nim in list_nim:
-        found = False
-        for data in data_mhs:
+
+class Mahasiswa:
+    def __init__(self, data_mhs):
+        self.data_mhs = data_mhs
+
+    def cari_mahasiswa(self, nim):
+        for data in self.data_mhs:
             if data["nim"] == nim:
-                mahasiswas_kelompok.append(data)
-                found = True
+                return data
+        return None
+
+
+class Kelompok:
+    def __init__(self):
+        self.data_kelompok = []
+
+    def tambah_kelompok(self, id_kelompok, instansi, lama_pkl, list_nim):
+        instansi_data = instansi.cari_instansi(nama_instansi)
+
+        if not instansi_data:
+            print("Instansi dengan nama tersebut tidak ditemukan.")
+            return
+
+        mahasiswas_kelompok = []
+        for nim in list_nim:
+            mahasiswa_data = mahasiswa.cari_mahasiswa(nim)
+            if mahasiswa_data:
+                mahasiswas_kelompok.append(mahasiswa_data)
+            else:
+                print(f"Mahasiswa dengan NIM {nim} tidak ditemukan.")
+
+        self.data_kelompok.append({
+            "id_kelompok": id_kelompok,
+            "nama_instansi": instansi_data["nama_instansi"],
+            "lama_pkl": lama_pkl,
+            "list_nim": [mhs["nim"] for mhs in mahasiswas_kelompok]
+        })
+
+        print("Data kelompok berhasil ditambahkan.")
+
+    def lihat_kelompok(self):
+        for kelompok in self.data_kelompok:
+            print("=" * 20)
+            print("ID Kelompok:", kelompok["id_kelompok"])
+            print("Nama Instansi:", kelompok["nama_instansi"])
+            print("Lama PKL:", kelompok["lama_pkl"], " Bulan")
+            print("NIM dalam Kelompok:", kelompok["list_nim"])
+            print("=" * 20)
+            print("")
+
+
+class MenuKelompok:
+    def __init__(self, mahasiswa, instansi, kelompok):
+        self.mahasiswa = mahasiswa
+        self.instansi = instansi
+        self.kelompok = kelompok
+
+    def menu_kelompok(self):
+        while True:
+            print("\n===== Menu Kelola Data Kelompok =====")
+            print("1. Tambah Kelompok")
+            print("2. Lihat Kelompok")
+            print("3. Kembali ke Menu Admin")
+            pilihan = input("Pilih menu: ")
+
+            if pilihan == "1":
+                lihat_mahasiswa()
+                lihat_instansi()
+                id_kelompok = input("ID Kelompok: ")
+                instansi_nama = input("Nama Instansi : ")
+                lama_pkl = input("Lama PKL: ")
+                list_nim = input("NIM dalam Kelompok (pisahkan dengan koma): ").split(",")
+                self.kelompok.tambah_kelompok(id_kelompok, self.instansi, lama_pkl, list_nim)
+            elif pilihan == "2":
+                self.kelompok.lihat_kelompok()
+            elif pilihan == "3":
                 break
-        if not found:
-            print(f"Mahasiswa dengan NIM {nim} tidak ditemukan.")
-
-    data_kelompok.append({
-        "id_kelompok": id_kelompok,
-        "nama_instansi": instansi["nama_instansi"],
-        "lama_pkl": lama_pkl,
-        "list_nim": [mhs["nim"] for mhs in mahasiswas_kelompok]
-    })
-
-    print("Data kelompok berhasil ditambahkan.")
+            else:
+                print("Pilihan tidak valid. Silakan coba lagi.")
 
 
-# Fungsi untuk melihat data kelompok
-def lihat_kelompok():
-    for kelompok in data_kelompok:
-        print("="*20)
-        print("ID Kelompok:", kelompok["id_kelompok"])
-        print("Nama Instansi:", kelompok["nama_instansi"])
-        print("Lama PKL:", kelompok["lama_pkl"]," Bulan")
-        print("NIM dalam Kelompok:", kelompok["list_nim"])
-        print("="*20)
-        print("")
+if __name__ == "__main__":
+    data_instansi = []  # Isi data instansi sesuai kebutuhan
+    data_mhs = []  # Isi data mahasiswa sesuai kebutuhan
 
-# Fungsi menu untuk mengelola data kelompok
-def menu_kelompok():
+    instansi = Instansi(data_instansi)
+    mahasiswa = Mahasiswa(data_mhs)
+    kelompok = Kelompok()
+    menu_kelompok = MenuKelompok(mahasiswa, instansi, kelompok)
+
     while True:
-        print("\n===== Menu Kelola Data Kelompok =====")
-        print("1. Tambah Kelompok")
-        print("2. Lihat Kelompok")
-        print("3. Kembali ke Menu Admin")
+        print("===== Menu Admin =====")
+        print("1. Kelola Data Mahasiswa")
+        print("2. Kelola Data Instansi")
+        print("3. Kelola Data Kelompok")
+        print("4. Keluar")
         pilihan = input("Pilih menu: ")
 
         if pilihan == "1":
-            lihat_mahasiswa()
-            lihat_instansi()
-            id_kelompok = input("ID Kelompok: ")
-            nama_instansi = input("Nama Instansi : ")
-            lama_pkl = input("Lama PKL: ")
-            list_nim = input("NIM dalam Kelompok (pisahkan dengan koma): ").split(",")
-            tambah_kelompok(id_kelompok, nama_instansi,lama_pkl, list_nim)
+            ms.menu_mahasiswa()
         elif pilihan == "2":
-            lihat_kelompok()
+            ins.menu_instansi()
         elif pilihan == "3":
+            menu_kelompok.menu_kelompok()
+        elif pilihan == "4":
             break
         else:
             print("Pilihan tidak valid. Silakan coba lagi.")
